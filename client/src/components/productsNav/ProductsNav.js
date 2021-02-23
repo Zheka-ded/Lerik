c17e910fa9fdd75e4da8d25a6312050dfd2d59b0
Zheka-ded <11vitamin11@rambler.ru>
2021-02-23 07:56:03 +0200

    разбираюсь с выводом категорий товара

--- client/src/components/productsNav/ProductsNav.js
+++ client/src/components/productsNav/ProductsNav.js
@@ -1,17 +1,44 @@
+// import { useEffect, useState } from 'react';
 import './ProductsNav.scss';
 
 export default function ProductsNav (props) {
 
     const { products } = props;
 
-    console.log(products)
+    // const [cat, setCat] = useState(null)
+
+    console.log(products);
+
+    let productsSet = new Set();
+
+    products?.map(elem => productsSet.add(elem.product));
+
+    console.log(productsSet);
+    productsSet = [...productsSet];
+    
+    // setCat(productsSet)
+
+    // useEffect(() => {
+    // },[])
+
+    console.log(productsSet);
 
     return (
         <div className="ProductsNav">
+            <p className="ProductsNav__title"> Категории товаров </p>
 
-            {products !== null && products.map(elem => (
-                <div key={elem._id} ><h2>{elem.title}</h2></div>
-            ))}
+            <ul>
+                {productsSet && productsSet.map(elem => (
+                    <li key={elem} ><button>{elem}</button></li>
+                ))}
+            </ul>
+
+            {/* <ul>
+                {cat !== null && cat.map(elem => (
+                    <li key={elem} ><button>{elem}</button></li>
+                ))}
+            </ul> */}
+
 
         </div>
     )
